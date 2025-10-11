bb58da1f73e18b63321a2c4993801fe95f687a08
Nikolas Jimenez Sanchez <njimenezs@javerianacali.edu.co>
2025-10-10 20:15:14 -0500

    modificacion dia antes

--- avance 27/project_1/project_1.srcs/sources_1/new/procesadorMips.vhd
+++ avance 27/project_1/project_1.srcs/sources_1/new/procesadorMips.vhd
@@ -35,6 +35,117 @@ end procesadorMips;
 architecture Behavioral of procesadorMips is
 
 begin
-
-
+process(reset,datain,clock)
+    begin 
+        if (reset = '1') then
+           dataout <= x"00000000";
+           addres <= x"00000000";
+           readEnable <= '0';
+           writeEnable <= '0';
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "000001") then
+            dataout <= x"00000000";
+           addres <= datain;
+           readEnable <= '1';
+           writeEnable <= '0';
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "000010") then
+            dataout <= x"00000000";
+           addres <= datain;
+           readEnable <= '0';
+           writeEnable <= '1';
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "000011") then
+           dataout <= x"00000000";
+           addres <= x"00000000";
+           readEnable <= '0';
+           writeEnable <= '0';
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "000100") then
+            dataout <= x"00000000";
+           addres <= x"00000000";
+           readEnable <= '0';
+           writeEnable <= '0';
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "000101") then
+            dataout <= x"00000000";
+           addres <= x"00000000";
+           readEnable <= '0';
+           writeEnable <= '0';
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "000110") then
+            dataout <= x"00000000";
+           addres <= x"00000000";
+           readEnable <= '0';
+           writeEnable <= '0';
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "000111") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "001000") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "001001") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "001010") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "001011") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "001100") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "001001") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "001010") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "001011") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "001100") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "001110") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "001111") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "010001") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "010010") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "010011") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "010100") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "010101") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "010110") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "010111") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "011000") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "011001") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "011010") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "011011") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "011100") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "011001") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "011010") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "011011") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "011100") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "011110") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "011111") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "100001") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "100010") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "100011") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "100100") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "100101") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "100110") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "100111") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "101000") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "101001") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "101010") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "101011") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "101100") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "101001") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "101010") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "101011") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "101100") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "101110") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "101111") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "110001") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "110010") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "110011") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "110100") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "110101") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "110110") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "110111") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "111000") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "111001") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "111010") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "111011") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "111100") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "111001") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "111010") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "111011") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "111100") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "111110") then
+        elsif(clock' event and clock = '1' and datain(31 downto 26) = "111111") then
+        else
+           dataout <= x"00000000";
+           addres <= x"00000000";
+           readEnable <= '0';
+           writeEnable <= '0';
+        
+        
+        
+        end if;
+    end process;
 end Behavioral;
